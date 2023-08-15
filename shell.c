bafadd7d16fcfeb87994ba6732dc83a9ca530404
jorge871 <jg303114@gmail.com>
2023-08-15 19:16:16 +0000

    Added the main to this project

---
+++ shell.c
@@ -0,0 +1,49 @@
+#include "main.h"
+/**
+* main - program shell
+* @argc: number of given arguments
+* @argv: array of arguments
+* Return:  return exit status of last program.
+*/
+int main(int __attribute__((unused)) argc, char **argv)
+{
+	char *buff = NULL, *cm;
+	size_t ln = 0;
+	char **args;
+	int status = 0;
+
+	while (1)
+	{
+		if (isatty(0) == 1)
+			_puts("$ ");
+		if (getline(&buff, &ln, stdin) == -1 || _strcmp(buff, "exit\n") == 0)
+		{
+			if (buff)
+				free(buff);
+			exit(status);
+		}
+		cm = _strdup(buff);
+		strtok(cm, "\n");
+		args = generate_arguments(cm, "\t \n");
+		free(cm);
+		if (_strcmp(args[0], "env") == 0)
+			_print_env();
+		else if (args[0])
+		{
+			cm = _which(args[0]);
+			if (cm)
+			{
+				free(args[0]);
+				args[0] = cm;
+				status = _execve(args);
+			}
+			else
+			{
+				status = 127;
+				_perror(argv[0], args[0]);
+			}
+		}
+		free_array(args);
+	}
+	return (0);
+}
